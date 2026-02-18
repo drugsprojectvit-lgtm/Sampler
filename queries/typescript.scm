e1abb3c7c774848763116993da4470622df4c147
drugsprojectvit-lgtm <drugsprojectvit@gmail.com>
2026-02-18 11:40:41 +0530

    Update typescript.scm

--- queries/typescript.scm
+++ queries/typescript.scm
@@ -1,7 +1,29 @@
+; Standard functions
 (function_declaration
-  name: (identifier) @function.name) @function.def
+  name: (identifier) @function.name
+  body: (statement_block) @function.body) @function.def
 
+; Classes
 (class_declaration
-  name: (identifier) @class.name) @class.def
+  name: (type_identifier) @class.name
+  body: (class_body) @class.body) @class.def
 
+; Interfaces (Treat as classes for the graph)
+(interface_declaration
+  name: (type_identifier) @class.name
+  body: (object_type) @class.body) @class.def
+
+; Methods
+(method_definition
+  name: (property_identifier) @function.name
+  body: (statement_block) @function.body) @function.def
+
+; Arrow functions
+(lexical_declaration
+  (variable_declarator
+    name: (identifier) @function.name
+    value: (arrow_function
+      body: (statement_block) @function.body))) @function.def
+
+; Imports
 (import_statement) @import
