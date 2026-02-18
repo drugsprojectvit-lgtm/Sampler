b697370f50277dc6f9374cca6e8b74e24a02c372
drugsprojectvit-lgtm <drugsprojectvit@gmail.com>
2026-02-18 11:40:03 +0530

    Update go.scm

--- queries/go.scm
+++ queries/go.scm
@@ -1,8 +1,23 @@
+; Functions: func Foo() {}
 (function_declaration
-  name: (identifier) @function.name) @function.def
+  name: (identifier) @function.name
+  body: (block) @function.body) @function.def
+
+; Methods: func (s *Struct) Foo() {}
+(method_declaration
+  name: (field_identifier) @function.name
+  body: (block) @function.body) @function.def
+
+; Structs and Interfaces (The "Classes" of Go)
+(type_declaration
+  (type_spec
+    name: (type_identifier) @class.name
+    type: (struct_type))) @class.def
 
 (type_declaration
   (type_spec
-    name: (type_identifier) @class.name)) @class.def
+    name: (type_identifier) @class.name
+    type: (interface_type))) @class.def
 
+; Imports
 (import_declaration) @import
