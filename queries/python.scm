e2dc0259db2bf627931d72fb5e4c6051f868d74e
drugsprojectvit-lgtm <drugsprojectvit@gmail.com>
2026-02-18 11:39:23 +0530

    Update python.scm

--- queries/python.scm
+++ queries/python.scm
@@ -1,9 +1,13 @@
+; Standard function definitions
 (function_definition
   name: (identifier) @function.name
   body: (block) @function.body) @function.def
 
+; Class definitions
 (class_definition
-  name: (identifier) @class.name) @class.def
+  name: (identifier) @class.name
+  body: (block) @class.body) @class.def
 
+; Imports
 (import_statement) @import
 (import_from_statement) @import
