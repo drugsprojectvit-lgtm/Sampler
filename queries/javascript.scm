a3579dd99d53f4279650efa1fb1ecd4bcf4174ec
drugsprojectvit-lgtm <drugsprojectvit@gmail.com>
2026-02-18 11:39:40 +0530

    Update javascript.scm

--- queries/javascript.scm
+++ queries/javascript.scm
@@ -1,7 +1,24 @@
+; Standard functions: function foo() {}
 (function_declaration
-  name: (identifier) @function.name) @function.def
+  name: (identifier) @function.name
+  body: (statement_block) @function.body) @function.def
 
+; Class definitions: class Foo {}
 (class_declaration
-  name: (identifier) @class.name) @class.def
+  name: (identifier) @class.name
+  body: (class_body) @class.body) @class.def
 
+; Methods inside classes: class Foo { bar() {} }
+(method_definition
+  name: (property_identifier) @function.name
+  body: (statement_block) @function.body) @function.def
+
+; Arrow functions assigned to variables: const foo = () => {}
+(lexical_declaration
+  (variable_declarator
+    name: (identifier) @function.name
+    value: (arrow_function
+      body: (statement_block) @function.body))) @function.def
+
+; Imports
 (import_statement) @import
